39baacc8956bb2dcde0814ccf8745e68e9e08db7
elduartte <el@duartte.co>
2020-12-16 17:59:32 +0100

    clase 2 generando secuencia de numeros

--- main.js
+++ main.js
@@ -6,13 +6,26 @@ const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Juego {
   constructor() {
-    this.iniciarlizar();
+    this.inicializar();
+    this.generarSecuencia();
   }
-  iniciarlizar() {
+  inicializar() {
     btnEmpezar.classList.add("hide");
+    this.nivel = 1;
+    this.colores = {
+      celeste,
+      violeta,
+      naranja,
+      verde,
+    };
+  }
+  generarSecuencia() {
+    this.secuencia = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * 4));
   }
 }
 
 function empezarJuego() {
-  var juego = new Juego();
+  window.juego = new Juego();
 }
