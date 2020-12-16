1c5adb82c27a1aae10389b0ca73b59d87441c242
elduartte <el@duartte.co>
2020-12-16 21:50:59 +0100

    clase 6 estados finales del juego

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const ULTIMO_NIVEL = 10;
 
 class Juego {
   constructor() {
+    this.inicializar = this.inicializar.bind(this);
     this.inicializar();
     this.generarSecuencia();
 
@@ -15,7 +16,8 @@ class Juego {
   inicializar() {
     this.siguienteNivel = this.siguienteNivel.bind(this);
     this.elegirColor = this.elegirColor.bind(this);
-    btnEmpezar.classList.add("hide");
+    this.toggleBtnEmpezar();
+
     this.nivel = 1;
     this.colores = {
       celeste,
@@ -24,6 +26,14 @@ class Juego {
       verde,
     };
   }
+  toggleBtnEmpezar() {
+    if (btnEmpezar.classList.contains("hide")) {
+      btnEmpezar.classList.remove("hide");
+    } else {
+      btnEmpezar.classList.add("hide");
+    }
+  }
+
   generarSecuencia() {
     this.secuencia = new Array(ULTIMO_NIVEL)
       .fill(0)
@@ -102,14 +112,25 @@ class Juego {
         this.nivel++;
         this.eliminarEventosClick();
         if (this.nivel === ULTIMO_NIVEL + 1) {
-          // GANÓ
+          this.ganoElJuego();
         } else {
           setTimeout(this.siguienteNivel, 1500);
         }
       }
     } else {
-      // PERDIÓ
+      this.perdioElJuego();
     }
+  }
+  ganoElJuego() {
+    swal("Platzi", "Felicitaciones ganaste el juego!", "success").then(
+      this.inicializar
+    );
+  }
+  perdioElJuego() {
+    swal("Platzi", "Lo lamentamos, perdiste :( ", "error").then(() => {
+      this.eliminarEventosClick();
+      this.inicializar();
+    });
   }
 }
 
